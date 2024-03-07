a09cb3e075dc5ca85401abeadcf4b00dc55d9e92
Ermonin Dima <ermonindima0@gmail.com>
2024-03-07 23:26:15 +0300

    one☄️

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+git@github.com:erm0nin/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(cardName, cardLink, cardDelete) {
+  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+  cardItem.querySelector('.card__title').textContent = cardName;
+  cardItem.querySelector('.card__image').src = cardLink;
+  const deleteBtn = cardItem.querySelector('.card__delete-button');
+  deleteBtn.addEventListener('click', () => deleteCard(cardItem));
+  return cardItem;
+};
 
-// @todo: Функция создания карточки
+function deleteCard(cardItem) {
+  cardItem.remove();
+};
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function (card) {
+  const cardItem = createCard(card.name, card.link, deleteCard);
+  placesList.append(cardItem);
+});
